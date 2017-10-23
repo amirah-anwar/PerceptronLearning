6fe72bd06135c3de2264196ca18cf4e82f7271c4
Michelle Becerra <michel8@vt.edu>
2017-10-23 11:08:41 -0700

    Perceptron Learning Algo finished

--- perceptron.py
+++ perceptron.py
@@ -7,7 +7,7 @@ import random
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
-
+from sklearn.linear_model import Perceptron
 # ==============Perceptron Algorithm==================================
 #Linear classification
 #Extracts coordinates, labels from classification.txt
@@ -28,90 +28,41 @@ def main():
 
 	#pad coordinates to make N x d+1 dimensionality (2,000 x 4)
 	padded_coordinates = np.insert(coordinates, 0, 1, axis=1)
-	# print padded_coordinates, padded_coordinates.shape
 
 	#randomly choose weights for each coordinate having dx1 (4,)dimentionality
 	weights = np.random.uniform(low=0.0, high=0.4, size=(len(padded_coordinates[0])))
-	#print weights, weights.shape
+
 	wcalc =   perceptronLearning(padded_coordinates, weights, labels)
-	print wcalc
-	# plot_costs(padded_coordinates[:,1], padded_coordinates[:,2], labels)
-	# plot_weight_line(wcalc, 0.0, 0.9)
-	# plt.title('Results for Perceptron Learning')
-	# plt.show()
-	#print labels, labels.shape
-	#find final weights using perceptron algorithm
-	#np.set_printoptions(threshold='nan')
-	#print 'Calculating final weights using perceptron algorithm...'
-	#print 'Calculated weights', perceptron(padded_coordinates, weights, labels)
-	#perceptron(padded_coordinates, weights, labels)
-def perceptronLearning(x, w, y):
-	alpha = 0.1
-	constBroken = True
-	while constBroken:
-		constBroken = False
-		for i in range(len(x)):
+	print 'Weight vector for Perceptron Learning Algorithm: \n', wcalc
+
+# Implements the percetron learning algorithm from class
+# As long as there is a constraint, either add or subtract alpha*x(i)
+# Converge or stop the algorithm where the number of constriants == 0
+# Return the weight vector
+def perceptronLearning(x, weight, y):
+	#Alpha should be "small" enough
+	alpha = 0.01
+	#The constraints broken count
+	constraint = 1
+	#The weight vector, currently all randomized 
+	w = weight
+
+	#While there exist a constraint run the PLA
+	while constraint != 0:
+		constraint = 0
+		#Randomly choose indexes from our data x
+		for i in np.random.permutation(range(len(x))):
 			y_calc = np.dot(w, x[i])
-			if(y_calc <= 0 and y[i] == 1):
-				constBroken = True
+			#Check wheter w*x violates a constraint
+			if(y_calc < 0 and y[i] == 1):
+				constraint += 1
 				w = np.add(w, alpha*x[i])
-			elif(y_calc >= 0 and y[i] == -1):
-				constBroken = True
+				break #Optimization, if there is already a broken constraint
+			elif(y_calc > 0 and y[i] == -1):
+				constraint += 1
 				w = np.subtract(w, alpha*x[i])
+				break #Optimization, if there is already a broken constraint
 	return w
-#Calculates violated constraints
-#Updates respective weights of coordinates which violate constraints
-#Keeps on updating weights untill there are no more violated constraints
-def perceptron(padded_coordinates, weights, labels):
-	learning_rate = 0.1
-
-	#find violated constraints
-	violations = calculateViolations(padded_coordinates, weights, labels)
-
-	#while there are violated constraints keep on changing weights
-	while len(violations['+ve']) != 0 or len(violations['-ve']) != 0:
-
-		#add learning rate in wieght if (w.T).coordinate < 0 and label is +ve
-		if len(violations['+ve']) != 0:
-			index = int(violations['+ve'][0][1])
-			weights[index] = np.add(weights[index], learning_rate*padded_coordinates[index])
-
-		#subtract learning rate in wieght if (w.T).coordinate > 0 and label is -ve
-		elif len(violations['-ve']) != 0:
-			index = int(violations['-ve'][0][1])
-			weights[index] = np.subtract(weights[index], learning_rate*padded_coordinates[index])
-
-		violations = calculateViolations(padded_coordinates, weights, labels)
-
-	return weights
-
-#Stores the result that violates constraints
-def calculateViolations(coordinates, weights, labels):
-	index = 0
-	violations = {}
-	pos_viol = []
-	neg_viol = []
-
-	for coordinate in coordinates:
-		result = np.dot((weights[index].T),coordinate)
-		if result < 0 and labels[index] == 1:
-			pos_viol.append(np.array([result, index]))
-		elif result > 0 and labels[index] == -1:
-			neg_viol.append(np.array([result, index]))
-		index += 1
-	
-	violations['+ve'] = np.array(pos_viol)
-	violations['-ve'] = np.array(neg_viol)
-	return violations
-def plot_costs(x0, x1, y):
-    for i, c in enumerate(y):  
-        plt.scatter(x0[i], x1[i], marker='+' if c==1 else '$-$',
-                    c='b', s=50)
-def plot_weight_line(weights, x0, x1):
-    def eq(w, x):
-        """ convert w0 + w1*x + w2*y into y = mx + b"""
-        return (-w[1]*x - w[0]) / w[2]
-    plt.plot([x0, x1], [eq(weights, x0), eq(weights, x1)], ls='--', color='g')
 
 if __name__ == "__main__":
     main()
